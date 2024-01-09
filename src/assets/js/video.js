2271a44029d972534be2c908b75b6dad78c8c30c
Matthias <matthias-endler@gmx.net>
2024-01-09 12:06:55 +0100

    Make video work on tiny screens
    Also adds escape button for the tiniest of screens.

--- src/assets/js/video.js
+++ src/assets/js/video.js
@@ -1,15 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document
-    .getElementById("playButton")
-    .addEventListener("click", function () {
-      console.log("click");
-      document.getElementById("videoModal").style.display = "block";
-      document.getElementById("videoPlayer").play();
-    });
+  const videoModal = document.getElementById("videoModal");
+  const videoPlayer = document.getElementById("videoPlayer");
 
-  document.getElementById("videoModal").addEventListener("click", function () {
-    console.log("click modal");
-    this.style.display = "none";
-    document.getElementById("videoPlayer").pause();
+  document.getElementById("playButton").addEventListener("click", function () {
+    console.log("click");
+    videoModal.style.display = "block";
+    videoPlayer.play();
+  });
+
+  document.getElementById("closeButton").addEventListener("click", function () {
+    videoModal.style.display = "none";
+    videoPlayer.pause();
+  });
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      videoModal.style.display = "none";
+      videoPlayer.pause();
+    }
+  });
+
+  videoModal.addEventListener("click", function (event) {
+    if (event.target === videoModal) {
+      console.log("click modal");
+      this.style.display = "none";
+      videoPlayer.pause();
+    }
   });
 });
